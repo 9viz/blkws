c707c44670a690fc11706b9367f8b0593f4b03fe
viz <viz@airmail.cc>
2019-03-19 22:37:04 +0530

    Actually commit changes to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,14 @@
 use std::{fs, process, io::Write};
+pub mod rand;
+pub mod util;
 
-fn grab_websitelinks<'a>(links: &'a str, website: &'a str) -> Vec<&'a str> {
-    /*
-     * links is the entire content of the links file
-     *
-     * grab all the links associated with website
-     */
-
-    let links: Vec<&str> = links.lines().collect();
-    let nlinks: usize = links.len() - 1;
-
-    let ind_website = links.iter()
-        .position(|&x| x == format!("[{}]", website))
-        .unwrap_or_else(|| {
-            process::exit(1);
-        });
-
-    let ind_end = links[ind_website..nlinks].iter()
-        .position(|&x| &x == &"")
-        .unwrap_or_else(|| {
-            process::exit(1);
-        });
-
-    return links[ind_website+1..ind_end].to_vec();
-}
-
-fn mkblockstring(website_links: &Vec<&str>) -> String {
-    /*
-     * block websitelinks in /etc/hosts
-     */
-
-    let mut content_hosts = String::new();
-
-    for l in website_links.iter() {
-        content_hosts.push_str(
-            &format!("0.0.0.0\t{}\n", l)
-        );
-    }
-
-    return content_hosts;
-}
-
-fn block(block_string: &String) {
+pub fn block(links_path: &str, website: &str) {
     /*
      * write to /etc/hosts
      */
+    let links = util::get_links(links_path);
+    let links = util::grab_websitelinks(links.as_str(), website);
+    let block_string = util::mkblockstring(&links);
 
     let host_file: &str = "/etc/hosts";
 
@@ -63,41 +27,52 @@ fn block(block_string: &String) {
         });
 }
 
+pub fn unblock(links_path: &str, website: &str) {
+    /*
+     * unblock a website
+     */
+    if ! util::puzzle() {
+        println!("You didn't win the puzzle! Can't unblock {}", website);
+        process::exit(1);
+    }
+
+    let links = util::get_links(links_path);
+
+    let blocked_websites: Vec<&str> = util::grab_websitelinks(
+        links.as_str(),
+        website
+    );
+
+    let mut new_hostfile: String = String::new();
+
+    let cur_hostfile = fs::read_to_string(&"/etc/hosts")
+        .unwrap_or_else(|e| {
+            eprintln!("error: {}", e);
+            process::exit(1);
+        });
+
+    let cur_hostfile: Vec<&str> = cur_hostfile.lines()
+        .collect();
+
+    for line in cur_hostfile.iter() {
+        if ! blocked_websites.contains(&line) {
+            new_hostfile.push_str(line);
+        }
+    }
+
+    fs::write(&"/etc/hosts", new_hostfile.as_str())
+        .unwrap_or_else(|e| {
+            eprintln!("error: {}", e);
+            process::exit(1);
+        });
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::fs;
 
     #[test]
-    fn grab_linktest() {
-        let contents = fs::read_to_string(
-            "/home/viz/usr/src/rust/blkhst/src/template"
-        ).unwrap();
-
-        println!("{:?}",
-                 grab_websitelinks(&contents, "website")
-                 );
-    }
-
-    #[test]
-    fn block_test() {
-        let contents = fs::read_to_string(
-            "/home/viz/usr/src/rust/blkhst/src/template"
-        ).unwrap();
-
-        let links = grab_websitelinks(&contents, "website");
-
-        println!("{}", mkblockstring(&links));
-    }
-
-    #[test]
-    fn write_test() {
-        let contents = fs::read_to_string(
-            "/home/viz/usr/src/rust/blkhst/src/template"
-        ).unwrap();
-
-        let links = grab_websitelinks(&contents, "website");
-
-        block(&mkblockstring(&links));
+    fn unblock_test() {
+        unblock(&"/home/viz/usr/src/rust/blkws/src/template", &"website");
     }
 }
